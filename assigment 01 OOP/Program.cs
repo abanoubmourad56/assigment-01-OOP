260c47323eefc52c0352c308b5694d7e92aee0fc
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-25 21:32:09 +0300

    Q2, Q3 pratical

--- assigment 01 OOP/Program.cs
+++ assigment 01 OOP/Program.cs
@@ -116,6 +116,99 @@ class Program
             */
 
 
+            // Q2 ,3 partical 
+            /*
+              public struct Shipment
+{
+    private string trackingCode;
+    private string description;
+    private double weight;
+    private decimal deliveryFee;
+
+    public string TrackingCode
+    {
+        get { return trackingCode; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+                description = value;
+        }
+    }
+
+    public double Weight
+    {
+        get { return weight; }
+        set
+        {
+            if (value > 0)
+                weight = value;
+        }
+    }
+
+    public decimal DeliveryFee
+    {
+        get { return deliveryFee; }
+        private set
+        {
+            if (value > 0)
+                deliveryFee = value;
+        }
+    }
+
+    public DeliveryAddress Destination { get; set; }
+
+    public decimal EstimatedCost
+    {
+        get { return DeliveryFee + ((decimal)Weight * 5); }
+    }
+
+    public Shipment(string trackingCode)
+    {
+        this.trackingCode = trackingCode;
+        this.description = "Unknown";
+        this.weight = 1;
+        this.deliveryFee = 50;
+        this.Destination = new DeliveryAddress("Unknown", "Unknown", 0);
+    }
+
+    public Shipment(
+        string trackingCode,
+        string description,
+        double weight,
+        decimal deliveryFee,
+        DeliveryAddress destination)
+    {
+        this.trackingCode = trackingCode;
+        this.description = string.IsNullOrWhiteSpace(description) ? "Unknown" : description;
+        this.weight = weight > 0 ? weight : 1;
+        this.deliveryFee = deliveryFee > 0 ? deliveryFee : 50;
+        this.Destination = destination;
+    }
+
+    public void UpdateDeliveryFee(decimal newFee)
+    {
+        if (newFee > 0)
+        {
+            deliveryFee = newFee;
+        }
+    }
+
+    public void PrintShipment()
+    {
+        Console.WriteLine($"Tracking Code: {TrackingCode}");
+        Console.WriteLine($"Description: {Description}");
+        Console.WriteLine($"Weight: {Weight}");
+        Console.WriteLine($"Delivery Fee: {DeliveryFee}");
+        Console.WriteLine($"Destination: {Destination.GetFullAddress()}");
+        Console.WriteLine($"Estimated Cost: {EstimatedCost}");
+    }
+}
+            */
         }
     }
 }
