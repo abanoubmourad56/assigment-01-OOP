9d6d405d51a554b7c992220eb88c3deb35d7763d
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-25 21:14:01 +0300

    question 1 b

--- assigment 01 OOP/Program.cs
+++ assigment 01 OOP/Program.cs
@@ -7,6 +7,8 @@
             //a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
             //DeliveryAddress is a struct (value type). When copied, a new independent copy is created.Modifying the copy does not affect the original variable.
 
+            //b) What happens when a Customer variable is copied into another variable and one variable modifies the object
+            //Customer is a class (reference type). When copied, both variables refer to the same object. Modifying the object through one variable affects the other variable.
 
         }
     }
