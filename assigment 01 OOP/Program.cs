b90748a0f8c4ccedce85ad2289553b6b5fa21133
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-25 21:12:20 +0300

    queestion 1

--- assigment 01 OOP/Program.cs
+++ assigment 01 OOP/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            
+            //a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+            //DeliveryAddress is a struct (value type). When copied, a new independent copy is created.Modifying the copy does not affect the original variable.
 
 
         }
